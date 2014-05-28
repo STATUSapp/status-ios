0c2cc7dbe2fb7c9ac24e3c371381caa274df0764
cosmin <cosmin@99022451-f8c5-4652-be14-efb92feb6bba>
2014-05-28 22:34:55 +0000

    growing textview done
    git-svn-id: svn://mobilep.ro/status@49 99022451-f8c5-4652-be14-efb92feb6bba

--- Status/STChatRoomViewController.m
+++ Status/STChatRoomViewController.m
@@ -9,8 +9,19 @@
 #import "STChatRoomViewController.h"
 #import "STMessageReceivedCell.h"
 #import "STMessageSendCell.h"
+#import "HPGrowingTextView.h"
 
-@interface STChatRoomViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface STChatRoomViewController ()<UITableViewDataSource, UITableViewDelegate, HPGrowingTextViewDelegate>
+{
+    NSMutableArray *_messages;
+}
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet HPGrowingTextView *textView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomTextViewConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
+@property (weak, nonatomic) IBOutlet UIButton *sendBtn;
+
 
 @end
 
@@ -20,7 +31,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
@@ -28,7 +39,119 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    _messages = [NSMutableArray new];
+    [self generateStringsWithNumber:@(20)];
+    [_tableView reloadData];
+    [_tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:_messages.count-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];    [self initiateCustomControls];
+}
+
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)initiateCustomControls {
+	    
+    _textView.isScrollable = YES;
+    _textView.contentInset = UIEdgeInsetsMake(0, 5, 0, 5);
+    _textView.font = [UIFont fontWithName:@"Helvetica Neue" size:16.f];
+	_textView.minNumberOfLines = 1;
+	_textView.maxNumberOfLines = 4;
+	_textView.returnKeyType = UIReturnKeySend; //just as an example
+	_textView.delegate = self;
+    _textView.internalTextView.scrollIndicatorInsets = UIEdgeInsetsMake(5, 0, 5, 0);
+    _textView.backgroundColor = [UIColor clearColor];
+    _textView.placeholder = @"Message";
+    
+}
+
+-(void)resignTextView
+{
+	[_textView resignFirstResponder];
+}
+
+//Code from Brett Schumann
+-(void) keyboardWillShow:(NSNotification *)note{
+    // get keyboard size and loctaion
+	CGRect keyboardBounds;
+    [[note.userInfo valueForKey:UIKeyboardFrameEndUserInfoKey] getValue: &keyboardBounds];
+    
+    // Need to translate the bounds to account for rotation.
+    keyboardBounds = [self.view convertRect:keyboardBounds toView:nil];
+    
+    
+	// get a rect for the textView frame
+	CGRect containerFrame = _containerView.frame;
+    containerFrame.origin.y = self.view.bounds.size.height - (keyboardBounds.size.height + containerFrame.size.height);
+
+	// animations settings
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:[note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+    [UIView setAnimationCurve:[note.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    _containerView.frame = containerFrame;
+    [UIView commitAnimations];
+    _bottomTextViewConstraint.constant = keyboardBounds.size.height;
+    
+
+}
+
+-(void) keyboardWillHide:(NSNotification *)note{
+	// get a rect for the textView frame
+	CGRect containerFrame = _containerView.frame;
+    containerFrame.origin.y = self.view.bounds.size.height - containerFrame.size.height;     
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:[note.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+    [UIView setAnimationCurve:[note.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    _containerView.frame = containerFrame;
+    [self.view layoutIfNeeded];
+    [UIView commitAnimations];
+    _bottomTextViewConstraint.constant = 0;
+}
+
+- (void)growingTextView:(HPGrowingTextView *)growingTextView willChangeHeight:(float)height
+{
+    float diff = (growingTextView.frame.size.height - height);
+    CGRect r = _containerView.frame;
+    r.size.height -= diff;
+    r.origin.y += diff;
+    [UIView animateWithDuration:0.25 animations:^{
+        _heightConstraint.constant = height;
+        //_bottomTextViewConstraint.constant =
+        //_containerView.frame = r;
+    }];
+	 
+}
+
+-(void)growingTextViewDidChange:(HPGrowingTextView *)growingTextView{
+    _sendBtn.enabled = _textView.text.length>0;
+}
+
+-(void)growingTextViewDidEndEditing:(HPGrowingTextView *)growingTextView{
+    if (growingTextView.text.length > 0) {
+        [self onSendButtonPressed:nil];
+    }
+    
+}
+
+-(BOOL)growingTextViewShouldReturn:(HPGrowingTextView *)growingTextView{
+    return [_textView resignFirstResponder];
+}
+- (IBAction)onSendButtonPressed:(id)sender {
+    [_messages addObject:_textView.text];
+    [_textView setText:@""];
+    [_textView resignFirstResponder];
+    
+    [_tableView reloadData];
+    [_tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:_messages.count-1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,21 +169,20 @@
         
 }
 
--(NSString *)getStringForIndexPath:(NSIndexPath *)indexPath{
+-(void)generateStringsWithNumber:(NSNumber *)nr{
     NSString *str = @"Message ";
     
-    for (int i=0; i<indexPath.row; i++) {
+    for (int i=0; i<nr.intValue; i++) {
         str = [str stringByAppendingString:@"asafd "];
+        [_messages addObject:[str copy]];
     }
-    
-    return str;
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     NSString *identifier = [self getIdentifierForIndexPath:indexPath];
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    NSString *message = [self getStringForIndexPath:indexPath];
+    NSString *message = [_messages objectAtIndex:indexPath.row];
     if (indexPath.row%2==0) {
         [(STMessageReceivedCell *)cell configureCellWithMessage:message];
     }
@@ -73,7 +195,7 @@
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    NSString *message = [self getStringForIndexPath:indexPath];
+    NSString *message = [_messages objectAtIndex:indexPath.row];
     if (indexPath.row%2 ==0 ) {
         return [STMessageReceivedCell cellHeightForText:message];
     }
@@ -85,7 +207,11 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    return 30;
+    return _messages.count;
+}
+
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
