bb84a4f3233b14b20fd4035dc073ea90c2c4f3b5
silviu <silviu@99022451-f8c5-4652-be14-efb92feb6bba>
2014-09-07 21:12:23 +0000

    started settings integration
    git-svn-id: svn://mobilep.ro/status@137 99022451-f8c5-4652-be14-efb92feb6bba

---
+++ trunk/Status/STSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  STSettingsViewController.h
+//  Status
+//
+//  Created by S B on 07/09/14.
+//  Copyright (c) 2014 Andrus Cosmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STSettingsViewController : UITableViewController
+
+@end
