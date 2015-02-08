7368916f87b51c38a1ed943bbfb651a38bbdf514
cosmin <cosmin@99022451-f8c5-4652-be14-efb92feb6bba>
2015-02-08 19:48:46 +0000

    add tutorial on login screen correct the 320 width on all app import most of the resources on 3x size.
    git-svn-id: svn://mobilep.ro/status@251 99022451-f8c5-4652-be14-efb92feb6bba

--- trunk/Status/STLoginViewController.m
+++ trunk/Status/STLoginViewController.m
@@ -13,7 +13,9 @@
 #import "STFacebookLoginController.h"
 #import "STNetworkQueueManager.h"
 
-@interface STLoginViewController ()
+@interface STLoginViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIPageControl *pageController;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @end
 
 @implementation STLoginViewController
@@ -44,7 +46,7 @@
                                            toItem:self.view
                                            attribute:NSLayoutAttributeBottom
                                            multiplier:1.f
-                                           constant:-102];
+                                           constant:-65];
     NSLayoutConstraint *centerConstraint = [NSLayoutConstraint constraintWithItem:loginBtn
                                                                         attribute:NSLayoutAttributeCenterX
                                                                         relatedBy:NSLayoutRelationEqual
@@ -54,6 +56,18 @@
                                                                          constant:1.f];
     
      [self.view addConstraints:@[bottomConstraint, centerConstraint]];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGSize size =  CGSizeMake(screenWidth, screenRect.size.height-158);
+    
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flowLayout setMinimumInteritemSpacing:0.0f];
+    [flowLayout setMinimumLineSpacing:0.0f];
+    [flowLayout setItemSize:size];
+    [self.collectionView setPagingEnabled:YES];
+    [self.collectionView setCollectionViewLayout:flowLayout];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -62,4 +76,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UICollectionViewDataSource
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+
+#pragma mark - UICollectionViewDelegate
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *identifier = [NSString stringWithFormat:@"Tutorial%ld", (long)indexPath.row];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    return cell;
+}
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 5;
+}
+#pragma mark - UIScrollViewDelegate
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    NSInteger currentpage = scrollView.contentSize.width/scrollView.contentOffset.x;
+    [_pageController setCurrentPage:currentpage];
+}
 @end
