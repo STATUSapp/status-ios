b20e0822c6bb3cc550fe82d749ed9c16514b00e2
Silviu Burlacu <silviu_burlacu@yahoo.com>
2016-03-04 17:01:06 +0200

    continued with tab bar structure

--- Status/UI/TabBar/STTabBarViewController.m
+++ Status/UI/TabBar/STTabBarViewController.m
@@ -9,6 +9,7 @@
 #import "STTabBarViewController.h"
 #import "STFlowTemplateViewController.h"
 #import "STSettingsViewController.h"
+#import "STTakeAPhotoViewController.h"
 
 static NSString * storyboardIdentifier = @"tabBarController";
 
@@ -29,36 +30,37 @@ static NSString * storyboardIdentifier = @"tabBarController";
 #pragma mark - Lifecycle
 
 - (void)setupTabBar {
-    STFlowTemplateViewController * homeFlow = [STFlowTemplateViewController getFlowControllerWithFlowType:STFlowTypeHome];
-    UINavigationController   * homeNav = [[UINavigationController alloc] initWithRootViewController:homeFlow];
-    homeNav.navigationBarHidden = YES;
+    
+    // add home flow
+    
+    UIViewController * homeVc = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateInitialViewController];
+    homeVc.title = @"Home";
+    
+    // add explore flow
+    
+    // add take a photo
+    
+    STTakeAPhotoViewController * takeAPhotoVC = [STTakeAPhotoViewController newController];
+    takeAPhotoVC.title = @"Take a Photo";
+    
+    // add message / notifications
+    
+    // add my profile
+    
     UIStoryboard * storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
     STSettingsViewController * settingsCtrl = [storyboard instantiateViewControllerWithIdentifier: NSStringFromClass([STSettingsViewController class])];
     UINavigationController   * settingsNav = [[UINavigationController alloc] initWithRootViewController:settingsCtrl];
     settingsNav.navigationBarHidden = YES;
-    
-    [self setViewControllers:@[homeNav, settingsNav] animated:NO];
+    settingsNav.title = @"Settings";
+
+
+    [self setViewControllers:@[homeVc, takeAPhotoVC, settingsNav] animated:NO];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 
 
