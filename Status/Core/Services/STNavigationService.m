8fc36d00a506c63571be986d9f355d48edfec446
Silviu Burlacu <silviu_burlacu@yahoo.com>
2016-03-08 13:50:08 +0200

    moved imagePickerService on CoreManager

--- Status/Core/Services/STNavigationService.m
+++ Status/Core/Services/STNavigationService.m
@@ -25,7 +25,6 @@
     if (self) {
         
         _imagePickerController = [STImagePickerController new];
-        _imagePickerService = [STImagePickerService new];
         
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDidLoggedIn) name:kNotificationUserDidLoggedIn object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDidRegister) name:kNotificationUserDidRegister object:nil];
