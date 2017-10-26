ccec9649aae1c7e3460a3ae9ffb4a8e7198d4011
Cosmin Andrus <andrus.cosmin@yahoo.com>
2017-10-26 20:09:50 +0300

    WIP

---
+++ Status/UI/TagProducts/STTagProductsTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  STTagProductsTabBarController.h
+//  Status
+//
+//  Created by Cosmin Andrus on 26/10/2017.
+//  Copyright © 2017 Andrus Cosmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STTagProductsTabBarController : UITabBarController
+
+@end
