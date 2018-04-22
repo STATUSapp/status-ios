e8946df71d1a204a81f7e2e43aad1058f5fa8d54
Cosmin Andrus <andrus.cosmin@yahoo.com>
2018-04-22 20:02:22 +0300

    send logs when a request fails. separate loggedIn state from guestMode state.

---
+++ Status/Core/Network/Requests/STSendLogsReguest.h
@@ -0,0 +1,13 @@
+//
+//  STSendLogsReguest.h
+//  Status
+//
+//  Created by Cosmin Andrus on 20/04/2018.
+//  Copyright © 2018 Andrus Cosmin. All rights reserved.
+//
+
+#import "STBaseRequest.h"
+
+@interface STSendLogsReguest : STBaseRequest
+
+@end
---
+++ Status/Core/Network/Requests/STSendLogsReguest.m
@@ -0,0 +1,13 @@
+//
+//  STSendLogsReguest.m
+//  Status
+//
+//  Created by Cosmin Andrus on 20/04/2018.
+//  Copyright © 2018 Andrus Cosmin. All rights reserved.
+//
+
+#import "STSendLogsReguest.h"
+
+@implementation STSendLogsReguest
+
+@end
